04a8ccf0e81da46a707c0e9f0e7168f6a1e4e021
toomemag <149588356+toomemag@users.noreply.github.com>
2025-03-27 02:14:15 +0200

    Commit1

---
+++ src/main/java/org/example/airplanetickets/AirplaneTicketsApplication.java
@@ -0,0 +1,13 @@
+package org.example.airplanetickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplaneTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirplaneTicketsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/airplanetickets/AirplaneTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.airplanetickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirplaneTicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
